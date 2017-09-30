7a90abc5796d5195226c21b67eaf02e0c5fccfaa
DDil <bho7982@naver.com>
2017-09-30 21:41:30 +0900

    첫 업로드
    작업시작

---
+++ js/custom.js
@@ -0,0 +1,55 @@
+(function ($) {
+                
+    // Navigation scrolls
+    $(".navbar-nav li a").on('click', function(event) {
+        $('.navbar-nav li').removeClass('active');
+        $(this).closest('li').addClass('active');
+        var $anchor = $(this);
+        var nav = $($anchor.attr('href'));
+        if (nav.length) {
+        $('html, body').stop().animate({				
+            scrollTop: $($anchor.attr('href')).offset().top				
+        }, 1500, 'easeInOutExpo');
+        
+        event.preventDefault();
+        }
+    });
+    
+    // Add smooth scrolling to all links in navbar
+    $("a.mouse-hover, a.get-quote").on('click', function(event) {
+      var hash = this.hash;
+      if( hash ) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 1500, 'easeInOutExpo');
+      }
+    });
+})(jQuery);
+//보드
+$(document).ready(function(){
+
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+        
+        if(value == "all")
+        {
+            //$('.filter').removeClass('hidden');
+            $('.filter').show('1000');
+        }
+        else
+        {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+            
+        }
+    });
+    
+    if ($(".filter-button").removeClass("active")) {
+$(this).removeClass("active");
+}
+$(this).addClass("active");
+
+});
